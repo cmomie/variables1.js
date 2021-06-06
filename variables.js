195f143e324d822b92e0518d6638dfffc35601ad
cmomie <57623682+cmomie@users.noreply.github.com>
2021-06-06 11:40:22 -0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,11 @@
+let CelciusTemp = 32
+let Fahrenheit1 = (CelciusTemp * 9/5) + 32
+
+let FahrenheitTemp = 89.6
+let Celcius1 = (FahrenheitTemp -32) * 5/9
+
+console.log(CelciusTemp);
+console.log(FahrenheitTemp);
+
+console.log(Fahrenheit1);
+console.log(Celcius1);
